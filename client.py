be1a9d34b78e5c843f4cf8fc434c728463ca00ab
hongb2873 <hongb2@tcnj.edu>
2022-04-27 15:02:21 -0400

    Did a little clean up

--- client.py
+++ client.py
@@ -25,21 +25,10 @@ machine = EnigmaMachine.from_key_sheet(
        ring_settings=[1, 20, 11],
        plugboard_settings='AV BS CG DL FU HZ IN KM OW RX')
 
-# server's IP address
-# if the server is not on this machine, 
-# put the private (network) IP address (e.g 192.168.1.2)
-# SERVER_HOST = "127.0.0.1"
-SERVER_HOST = "159.91.93.242"
-#SERVER_HOST = "159.91.226.35"
-# SERVER_HOST = "159.91.93.188"
-# SERVER_HOST = "10.9.0.1"
-SERVER_PORT = 5002 # server's port
-separator_token = "<SEP>" # we will use this to separate the client name & message
 
 # server's IP address
 # if the server is not on this machine, 
 # put the private (network) IP address (e.g 192.168.1.2)
-
 def opening_inputs():
     server_host = ""
     print("Would you like to use a default server host or enter a private IP address?")
@@ -61,30 +50,39 @@ def opening_inputs():
         else:
             print("Invalid input. Please enter a valid option.")
 
+
+
+SERVER_HOST = opening_inputs()
+
+SERVER_PORT = 5002        # server's port
+separator_token = "<SEP>" # we will use this to separate the client name & message
+
+
 # initialize TCP socket
 s = socket.socket()
 print(f"[*] Connecting to {SERVER_HOST}:{SERVER_PORT}...")
 # connect to the server
 s.connect((SERVER_HOST, SERVER_PORT))
 print("[+] Connected.")
+
 # prompt the client for a name
 name = input("Enter your name: ")
-#start_pos = input("Enter the starting position: ")
-#msg_key = input("Enter the message key: ")
 
-startingPosition = input("Enter the intiial starting position: ")
 # set machine initial starting position
-machine.set_display(startingPosition)
+startingPosition = input("Enter the intiial starting position: ")
+
 #machine.set_display('WXC')
+machine.set_display(startingPosition)
 
 messageKey = input("Enter the message key: ")
+
 # decrypt the message key
-#msg_key = machine.process_text('KCH')
 msg_key = machine.process_text(messageKey)
 
 # decrypt the cipher text with the unencrypted message key
 machine.set_display(msg_key)
 
+print("Registered. You can now start typing for messages.")
 
 def listen_for_messages():
     while True:
@@ -94,11 +92,12 @@ def listen_for_messages():
         decoded = machine.process_text(message)
         print("\n" + decoded)
 
-#machine.set_display(msg_key)
 # make a thread that listens for messages to this client & print them
 t = Thread(target=listen_for_messages)
+
 # make the thread daemon so it ends whenever the main thread ends
 t.daemon = True
+
 # start the thread
 t.start()
 
