a510703d2078ab06a95876154799ce1f7ed0a536
nedkia <78882341+nedkia@users.noreply.github.com>
2022-05-06 15:34:24 -0400

    Update client.py

--- client.py
+++ client.py
@@ -30,34 +30,9 @@ machine = EnigmaMachine.from_key_sheet(
        ring_settings=[1, 20, 11],
        plugboard_settings='AV BS CG DL FU HZ IN KM OW RX')
 
-tprint("Enigma Chat")
-
-def opening_inputs():
-    server_host = ""
-    print("Would you like to use a default server host or enter a private IP address?")
-    
-    option = ""
-
-    while option != "1" or option != "2":
-        print("1: Default Server Host")
-        print("2: Enter private IP address")
-
-        option = input("")
-
-        if option == "1":
-            server_host = "127.0.0.1"
-            return server_host
-        elif option == "2":
-            server_host = input("Enter IP address: ")
-            return server_host
-        else:
-            print("Invalid input. Please enter a valid option.")
-
 # server's IP address
-# if the server is not on this machine, 
-# put the private (network) IP address (e.g 192.168.1.2)
-
-SERVER_HOST = opening_inputs()
+SERVER_HOST = "159.91.66.147"
+# SERVER_HOST = "10.9.0.1"
 SERVER_PORT = 5002 # server's port
 separator_token = "<SEP>" # we will use this to separate the client name & message
 
@@ -94,15 +69,15 @@ s.connect((SERVER_HOST, SERVER_PORT))
 print("[+] Connected.")
 
 print("\nWelcome to the supersecret Chatroom\n")
+tprint("Enigma Chat")
 print("\nDo not type any numbers as we do not understand them\n")
 
-# prompt the client for a name
-name = input("Enter your name: ")
 
-print("\nWelcome to the Chatroom\n")
 
 # prompt the client for a name
 name = input("Enter your name: ")
+#start_pos = input("Enter the starting position: ")
+#msg_key = input("Enter the message key: ")
 
 startingPosition = input("Enter the initial starting position: ")
 
@@ -113,6 +88,8 @@ if len(startingPosition) != 3:
 
 # set machine initial starting position
 machine.set_display(startingPosition)
+#machine.set_display('WXC')
+
 messageKey = input("Enter the message key: ")
 
 if len(messageKey) != 3:
@@ -121,15 +98,16 @@ if len(messageKey) != 3:
             messageKey = input("Enter the new message key: \n")
 
 # decrypt the message key
+#msg_key = machine.process_text('KCH')
 msg_key = machine.process_text(messageKey)
 
 # decrypt the cipher text with the unencrypted message key
 machine.set_display(msg_key)
 
+
+print("\nEncryption settings saved. Type and enter the 'e' key at any time to change encryption settings. \n")
+
 def menu(startingPosition,msg_key):
-
-    print("\nEncryption settings saved. Type and enter the 'e' key at any time to change encryption settings. \n")
-
     doneWithMenu = 0
 
     while doneWithMenu==0:
@@ -141,6 +119,7 @@ def menu(startingPosition,msg_key):
         userInput = input()
 
         if userInput == "1":
+            #print("\nYour current starting position is: " + startingPosition)
             startingPosition = input("\nEnter the new initial starting position: \n")
             if len(startingPosition) != 3:
                 while len(startingPosition) !=3 :
@@ -150,6 +129,7 @@ def menu(startingPosition,msg_key):
             print("\nInitial starting position successfully changed to " + startingPosition + "\n")
         
         elif userInput == "2":
+            #print("\nYour current message key is: " + messageKey)
             messageKey = input("\nEnter the new message key: \n")
             if len(messageKey) != 3:
                 while len(messageKey) !=3 :
@@ -161,20 +141,30 @@ def menu(startingPosition,msg_key):
         
         elif userInput == "3" :
             doneWithMenu = 1
-            print("Returned to chat")
+            #print("\nSuccessfully quit menu\n")
             return startingPosition,msg_key
+
+
+
+# while True:
+
+#     userInput = input()
+
+#     if userInput == "e":
+#        #print("you pressed e")
+#        menu()
 
 
 def listen_for_messages():
     while True:
         message = s.recv(1024).decode()
         var = message.split('TEST')
+        #message = s.recv(1024)
         machine.set_display(msg_key)
         decoded = machine.process_text(var[1]) 
         print("\n" + var[0] + decoded)
-        machine.set_display(msg_key)
-        decoded = machine.process_text(message)
-        print("\n" + decoded)
+
+#machine.set_display(msg_key)
 
 # make a thread that listens for messages to this client & print them
 t = Thread(target=listen_for_messages)
@@ -185,13 +175,18 @@ t.start()
        
 
 while True:
+
+    #if keyboard.read_key() == "e":
+        #print("you pressed e")
+    #    menu()
+
     # input message we want to send to the server
     to_send =  input()
 
     if to_send == "e":
        startingPosition, msg_key = menu(startingPosition,msg_key)
-
     else:
+
         # decrypt the cipher text with the unencrypted message key
         machine.set_display(msg_key)
 
